46064eb98ff550d0aab35ed74fc1ede2e58bcc76
Dang Huynh <danghuynh1526@gmail.com>
2024-08-28 20:33:14 +0700

    design_school_subject

--- src/pages/Subject/index.js
+++ src/pages/Subject/index.js
@@ -1,5 +1,115 @@
+import React, { useEffect, useState } from 'react';
+import { Table } from 'antd';
+import qs from 'qs';
+
+import styles from './Subject.module.scss';
+const columns = [
+    {
+        title: 'Name',
+        dataIndex: 'name',
+        sorter: true,
+        render: (name) => `${name.first} ${name.last}`,
+        width: '20%',
+    },
+    {
+        title: 'Gender',
+        dataIndex: 'gender',
+        filters: [
+            {
+                text: 'Male',
+                value: 'male',
+            },
+            {
+                text: 'Female',
+                value: 'female',
+            },
+        ],
+        width: '20%',
+    },
+    {
+        title: 'Email',
+        dataIndex: 'email',
+    },
+];
+const getRandomuserParams = (params) => ({
+    results: params.pagination?.pageSize,
+    page: params.pagination?.current,
+    ...params,
+});
+
 function Subject() {
-    return <h2>Subject page</h2>
+    const [data, setData] = useState();
+    const [loading, setLoading] = useState(false);
+    const [tableParams, setTableParams] = useState({
+        pagination: {
+            current: 1,
+            pageSize: 10,
+        },
+    });
+    const fetchData = () => {
+        setLoading(true);
+        fetch(`https://randomuser.me/api?${qs.stringify(getRandomuserParams(tableParams))}`)
+            .then((res) => res.json())
+            .then(({ results }) => {
+                setData(results);
+                setLoading(false);
+                setTableParams({
+                    ...tableParams,
+                    pagination: {
+                        ...tableParams.pagination,
+                        total: 200,
+                    },
+                });
+            });
+    };
+    useEffect(() => {
+        fetchData();
+    }, [
+        tableParams.pagination?.current,
+        tableParams.pagination?.pageSize,
+        tableParams?.sortOrder,
+        tableParams?.sortField,
+        JSON.stringify(tableParams.filters),
+    ]);
+    const handleTableChange = (pagination, filters, sorter) => {
+        setTableParams({
+            pagination,
+            filters,
+            sortOrder: Array.isArray(sorter) ? undefined : sorter.order,
+            sortField: Array.isArray(sorter) ? undefined : sorter.field,
+        });
+
+        if (pagination.pageSize !== tableParams.pagination?.pageSize) {
+            setData([]);
+        }
+    };
+
+    return (
+        <>
+            <h2>Subject page</h2>
+            <hr className={styles.line} />
+            <div className={styles['button-actions']}>
+                <div className={styles['crud']}>
+                    <button type="button" className="btn btn-primary">Add school</button>
+                    <button type="button" className="btn btn-success">Edit</button>
+                    <button type="button" className="btn btn-danger">Delete</button>
+                </div>
+            </div>
+            <Table
+                columns={columns}
+                rowKey={(record) => record.login.uuid}
+                dataSource={data}
+                pagination={tableParams.pagination}
+                loading={loading}
+                onChange={handleTableChange}
+            />
+        </>
+
+
+    )
 }
 
 export default Subject;
+
+
+
